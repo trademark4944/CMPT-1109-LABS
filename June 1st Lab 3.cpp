314624d4369d2b47971513012264652ecc569047
Mark <106276853+trademark4944@users.noreply.github.com>
2022-06-06 13:56:25 -0700

    Update June 1st Lab 3.cpp

--- June 1st Lab 3.cpp
+++ June 1st Lab 3.cpp
@@ -1,50 +1,38 @@
 #include <iostream>
 using namespace std;
 
-void drawTriangle(int size, char symbol) {
-	for (int i = 1; i <= size; i++) {
-		for (int j = 1; j <= (size - i); j++) {
+void drawTriangle(int size, char symbol) { //function which draws triangle with size and symbol taken from inputs from main()
+
+	for (int i = 1; i <= size; i++) { //increments number of lines needed in triangle (the size of the trienagle)
+		for (int j = 1; j <= (size - i); j++) { 
+			//displays needed spaces at beggining of each line, in order to create a right triangle with it's diagonal on the left 
 			cout << " ";
 		}
-		for (int k = 0; k < i; k++) {
+		for (int k = 0; k < i; k++) { //inputs the symbols for this left diagonal triangle
 			cout << symbol;
 		}
-		for (int n = 1; n < i; n++) {
-			cout << symbol;
-		}
-		cout << endl;
-	}
-	return;
-}
-void drawLeftTriangle(int size, char symbol) {
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j <= i; j++) {
+		for (int n = 1; n < i; n++) { //completes the other half of the triangle, creating the diagonal on the right
 			cout << symbol;
 		}
 		cout << endl;
 	}
+	/*for loops use variable i to determine how many symbols to output, i is dependent on which line is being created
+	(eg.line 1 has 1 input and line 2 gets 2 and so forth)*/
 	return;
 }
 
-void drawRightTriangle(int size, char symbol) {
-	for (int i = 1; i <= size; i++) {
-		for (int j = 1; j <= (size - i); j++) {
-			cout << " ";
+void drawDiamond(int size, char symbol) { //function which draws a diamond with size and symbol taken from inputs from main()
 
-		}
-		for (int n = 1; n <= i; n++) {
-			cout << symbol;
-		}
-		cout << endl;
-	}
-	return;
-}
+	drawTriangle(size, symbol); //calls triangle function as the top half of a diamond is a triangle, less code to write :)
 
-void drawDiamond(int size, char symbol) {
-	drawTriangle(size, symbol);
+	size--; /*size is decreased by 1 as the middle line of the diamond has already been created when we call drawTriangle(),
+			therefore we only need to draw an upside down triangle with 1 less line than the right side up one to finish the diamond*/
 
-	size--;
-	for (int i = 1; i <= size; i++) {
+	for (int i = 1; i <= size; i++) { 
+		/*the following for loops are similar to the for loops in drawTriangle(), only they are reversed 
+		instead of incrementing with an increase we increment with a decrease in order to draw an upside down triangle.
+		we draw the most ammount of symbols and then the least ammount of symbols thus why we start at the large number and then decrease
+		to the small number*/
 		for (int j = 0; j < i; j++) {
 			cout << " ";
 		}
@@ -65,8 +53,13 @@ int main() {
 	char symbol;
 	bool cont = true;
 
-	while (cont == true) {
-		cout << "Make selection: draw triangle(1) draw diamond(2) quit(3)  ";
+	while (cont == true) { //while loop to continue the function until user decides to exit program
+
+		cout << "Make selection: draw triangle(1) draw diamond(2) quit(3)  "; 
+		
+		/*variable choice and the switch case loop determines what the user wants to accomplish, the loop calls respective functions for each
+		task the user requests, it also gathers needed inputs for each function*/
+		
 		cin >> choice;
 
 		switch (choice) {
