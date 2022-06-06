9ef6a3651c6d599728f1ad42596118809899854b
Mark <106276853+trademark4944@users.noreply.github.com>
2022-06-06 13:38:36 -0700

    Update June 1st Lab 3.cpp

--- June 1st Lab 3.cpp
+++ June 1st Lab 3.cpp
@@ -1,26 +1,5 @@
 #include <iostream>
-#include <string>
 using namespace std;
-
-/* for (int i = 0; i < size; i++) {
-	for (int j = 0; j <= i; j++) {
-		cout << char;
-	}
-
-for (int i = 0; i < size; i++) {
-	for (int j = 1; j <= (size - 1); j++) {
-		cout << "_";
-	for (int n = 0; n <= i; n++) {
-		cout << char;
-
-
-	height = size
-	length = (size*2) - 1;
-	array [height, length]
-
-	for (int h = 0; h < height; h++) {
-		for (int k = 0; k < length; k++) {
-} */
 
 void drawTriangle(int size, char symbol) {
 	for (int i = 1; i <= size; i++) {
@@ -51,10 +30,10 @@ void drawRightTriangle(int size, char symbol) {
 	for (int i = 1; i <= size; i++) {
 		for (int j = 1; j <= (size - i); j++) {
 			cout << " ";
-			
+
 		}
 		for (int n = 1; n <= i; n++) {
-				cout << symbol;
+			cout << symbol;
 		}
 		cout << endl;
 	}
@@ -62,7 +41,23 @@ void drawRightTriangle(int size, char symbol) {
 }
 
 void drawDiamond(int size, char symbol) {
+	drawTriangle(size, symbol);
 
+	size--;
+	for (int i = 1; i <= size; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << " ";
+		}
+		for (int n = size; n >= i; n--) {
+			cout << symbol;
+		}
+		for (int k = (size - 1); k >= i; k--) {
+			cout << symbol;
+		}
+		cout << endl;
+	}
+
+	return;
 }
 
 int main() {
@@ -74,28 +69,28 @@ int main() {
 		cout << "Make selection: draw triangle(1) draw diamond(2) quit(3)  ";
 		cin >> choice;
 
-		switch (choice){
-			case 1:
-				cout << "Input size of shape: ";
-				cin >> size;
-				cout << "Input desired character: ";
-				cin >> symbol;
-				drawTriangle(size, symbol);
-				break;
-			case 2:
-				cout << "Input size of shape: ";
-				cin >> size;
-				cout << "Input desired character: ";
-				cin >> symbol;
-				drawDiamond(size, symbol);
-				break;
-			case 3:
-				cout << endl << "Program Exiting..." << endl;
-				cont = false;
-				break;
-			default: 
-				cout << "ERROR: Invalid Input" << endl;
-				break;
+		switch (choice) {
+		case 1:
+			cout << "Input size of shape: ";
+			cin >> size;
+			cout << "Input desired character: ";
+			cin >> symbol;
+			drawTriangle(size, symbol);
+			break;
+		case 2:
+			cout << "Input size of shape: ";
+			cin >> size;
+			cout << "Input desired character: ";
+			cin >> symbol;
+			drawDiamond(size, symbol);
+			break;
+		case 3:
+			cout << endl << "Program Exiting..." << endl;
+			cont = false;
+			break;
+		default:
+			cout << "ERROR: Invalid Input" << endl;
+			break;
 		}
 	}
 
