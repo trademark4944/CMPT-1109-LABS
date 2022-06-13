35be5cb8ee79bd5d779041c6e3f3306eb21cc1ad
Mark <106276853+trademark4944@users.noreply.github.com>
2022-06-13 13:54:27 -0700

    Update jue 8th lab 4.cpp

--- jue 8th lab 4.cpp
+++ jue 8th lab 4.cpp
@@ -18,80 +18,144 @@ bool checkLeapYear(int year) {
 	}
 }
 
-void printDates(int weekDayStart, string month) {
-	int dayCount = 1;
-	int calendarDates[5][7];
+/*int calculateWeekDayStart(int prevStartDay) {
+	int newStartDay = 31, hold;
 
-	for (int rows = 0; rows < 5; rows++) {
+	newStartDay += prevStartDay;
+	hold = newStartDay % 7;
+	newStartDay /= 7;
+	newStartDay + hold;
+
+	return newStartDay;
+}*/
+
+void printDates(int weekDayStart, string month, int dayMax) {
+	int dayCount = 1;
+	int calendarDates[6][7];
+
+	for (int rows = 0; rows < 6; rows++) {
 		for (int cols = 0; cols < 7; cols++) {
 			if (rows == 0 && cols < weekDayStart) {
 				calendarDates[rows][cols] = 0;
 			}
-			else { 
-                                 calendarDates[rows][cols] = dayCount;
-                                 dayCount++;
-			         if (dayCount > 31) {
-				     break;
-				 }
+			else {
+				calendarDates[rows][cols] = dayCount;
+				dayCount++;
+			}
+			if (dayCount > dayMax) {
+				break;
 			}
 		}
-		if (dayCount > 31) {
+		if (dayCount > dayMax) {
 			break;
 		}
 	}
 
-	dayCount = 1;
-	cout << month << endl << endl;
+	cout << endl << month << endl << endl;
 	cout << "SUN \t MON  \t  TUE \t WED \t THU \t FRI \t SAT" << endl;
 
-	for (int rows = 0; rows < 5; rows++) {
+	dayCount = 1;
+	for (int rows = 0; rows < 6; rows++) {
 		for (int cols = 0; cols < 7; cols++) {
-                        if (calenderDates[rows][cols] == 0) {
+			if (calendarDates[rows][cols] == 0) {
 				cout << " ";
-                        }
+			}
 			else {
 				cout << calendarDates[rows][cols];
 				dayCount++;
 			}
 			cout << '\t' << " ";
-			
-			if (dayCount > 31) {
+			if (dayCount > dayMax) {
 				break;
 			}
 		}
-		cout << endl;
-		if (dayCount > 31) {
+		if (dayCount > dayMax){
 			break;
 		}
+		cout << endl;
 	}
+	cout << endl;
 }
 
 
 //array[rows][cols]
-void generateMonth(int monthLength, int monthcount, int weekDayStart) {
+void generateMonth(int monthLength, int monthcount, int weekDayStart, bool isLeapYear) {
 	string month;
-	if (monthcount == 1) {
+	int dayMax = 0;
+	switch (monthcount) {
+	case 1:
 		month = "January";
-		printDates(weekDayStart, month);
+		dayMax = 31;
+		break;
+	case 2:
+		month = "Febuary";
+		if (isLeapYear == true) {
+			dayMax = 29;
+		}
+		else {
+			dayMax = 28;
+		}
+		break;
+	case 3:
+		month = "March";
+		dayMax = 31;
+		break;
+	case 4:
+		month = "April";
+		dayMax = 30;
+		break;
+	case 5:
+		month = "May";
+		dayMax = 31;
+		break;
+	case 6:
+		month = "June";
+		dayMax = 30;
+		break;
+	case 7:
+		month = "July";
+		dayMax = 31;
+		break;
+	case 8:
+		month = "August";
+		dayMax = 31;
+		break;
+	case 9:
+		month = "September";
+		dayMax = 30;
+		break;
+	case 10:
+		month = "October";
+		dayMax = 31;
+		break;
+	case 11:
+		month = "November";
+		dayMax = 30;
+		break;
+	case 12:
+		month = "Decemeber";
+		dayMax = 31;
+		break;
 	}
+	printDates(weekDayStart, month, dayMax);
 }
 
 int main() {
-	int year, januaryFirst, monthLength = 1, monthCount = 1;
+	int year, weekDayStart, monthLength = 1, dayMax;
 	bool leapYear, idiotProofing = true;
 
 	cout << "Enter the year for which you wish to generate the calendar: ";
 	cin >> year;
-	
+
 	while (idiotProofing == true) {
 		cout << "------------------------------------------------------------" << endl;
 		cout << "0. Sunday" << endl << "1. Monday" << endl << "2. Tuesday" << endl << "3. Wednesday" << endl << "4. Thursday" << endl << "5. Friday" << endl << "6. Saturday" << endl;
 		cout << "------------------------------------------------------------" << endl;
 		cout << "Enter the day of the week that January first is on: ";
-		cin >> januaryFirst;
+		cin >> weekDayStart;
 		cout << "------------------------------------------------------------" << endl;
 
-		if (januaryFirst > 7 || januaryFirst < 1) {
+		if (weekDayStart > 7 || weekDayStart < 0) {
 			cout << "ERROR: Invalid Input" << endl;
 		}
 		else {
@@ -100,30 +164,12 @@ int main() {
 	}
 
 	leapYear = checkLeapYear(year);
+
+	cout << "Calendar for " << year << endl;
 	
-	cout << "Calendar for " << year << endl << endl;
-
-	generateMonth(monthLength, monthCount, januaryFirst);
-
-	/*
-	for (int monthCount = 1; monthCount <= 12; monthCount++) {
-		if (monthCount == (1 || 3 || 5 || 7 || 8 || 10 || 12)) {
-			monthLength == 31;
-		}
-		else if (monthCount == 2) {
-			if (leapYear == true) {
-				monthLength = 29;
-			}
-			else if (leapYear == false) {
-				monthLength = 28;
-			}
-		}
-		else {
-			monthLength = 30;
-		}
-
-		generateMonth(monthLength, monthCount);
-	} */
+	for (int i = 1; i <= 12; i++) {
+		generateMonth(monthLength, i, weekDayStart, leapYear);
+	}
 
 	return 0;
 }
