d5c9e55a74339e9d7d61e6c8b8e516bd37ee1437
Mark <106276853+trademark4944@users.noreply.github.com>
2022-07-04 01:33:51 -0700

    Update june 29th in class.cpp

--- june 29th in class.cpp
+++ june 29th in class.cpp
@@ -1,38 +1,74 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
 using namespace std;
+fstream mainFile;
 
-int main() {
-	fstream mainFile;
-	bool fileOpen = true;
+bool isOpen() {
+	bool fileOpen;
+	if (mainFile.is_open()) {
+		fileOpen = true;
+	}
+	else {
+		cout << "ERROR: Unable to Open File." << endl;
+		fileOpen = false;
+	}
+	return fileOpen;
+}
+
+string getAnswer(bool &fileOpen, int counter) {
 	char chars;
 	string string;
+	int i = 0;
+
+	while (i < counter) {
+		mainFile.ignore(100, '.');
+		i++;
+	}
+
+	while (fileOpen) {
+		mainFile.get(chars);
+		if (chars == '.') {
+			mainFile.close();
+			fileOpen = mainFile.is_open();
+		}
+		else {
+			string += chars;
+		}
+	}
+
+	return string;
+}
+
+void writeAnswer(bool &fileOpen, string answer) {
+	mainFile << answer << "\n";
+	mainFile.close();
+	fileOpen = mainFile.is_open();
+}
+
+int main() {
+	bool isOutput, fileOpen = true;
+	string question, answer;
 	int counter = 0;
 
-	while (counter < 2) {
-		mainFile.open("answers.txt");
-		fileOpen = true;
+	while (question != "0") {
+		cout << "Enter a question or 0 to exit: ";
+		cin >> question;
+		counter++;
 		for (int i = 0; i < counter; i++) {
 			mainFile.ignore(500, '.');
 		}
-		while (fileOpen) {
-			mainFile.get(chars);
-			if (chars == '.') {
-				fileOpen = false;
-			}
-			else if (!mainFile.eof()) {
-				string += chars;
-			}
-			else {
-				fileOpen = false;
-			}
-		}
-		counter++;
-		mainFile.close();
+		mainFile.open("answers.txt");
+		fileOpen = isOpen();
 
-		mainFile.open("file.txt");
-		mainFile << string;
-		mainFile.close();
+		if (fileOpen == true) {
+			answer = getAnswer(fileOpen, counter);
+		}
+		else {
+			return 0;
+		}
+		writeAnswer(fileOpen, answer);
+		
 	}
 
 	return 0;
